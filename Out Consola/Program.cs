52ac39619c0a69ec7ca78cdca79ab8dbe32c7ae4
MerioUwU <54387601+MerioUwU@users.noreply.github.com>
2019-09-26 10:27:44 -0700

    Programa desde C#
    Copia directa de la carpeta de repositorios donde se hizo el programa

---
+++ Out Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalcents, pesos, centavos;
+            Console.WriteLine("Ingrese la cantidad de Centavos que quiere convertir a pesos: ");
+            totalcents = int.Parse(Console.ReadLine());
+            Convert(totalcents, out pesos, out centavos);
+            Console.WriteLine("La conversión resulta en {0} pesos con {1} centavos, presione cualquier tecla para salir",pesos,centavos);
+            Console.ReadKey();
+        }
+        static void Convert (int totalcents, out int pesos, out int centavos)
+        {
+            pesos = totalcents / 100;
+            centavos = totalcents % 100;
+        }
+    }
+}
